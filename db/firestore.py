daa4042facae58e864de0a3afda533680bfd18f1
joscha0 <joschavonandrian@gmail.com>
2020-09-22 18:28:30 +0200

    fix cert format error

--- db/firestore.py
+++ db/firestore.py
@@ -3,18 +3,20 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import os
 
-cred = credentials.Certificate({
+certificate = {
     "type": "service_account",
     "project_id": "daily-wiki-newsletter",
     "private_key_id": os.environ['private_key_id'],
-    "private_key": os.environ['private_key'],
+    "private_key": os.environ['private_key'].replace('\\n', '\n'),
     "client_email": os.environ['client_email'],
     "client_id": os.environ['client_id'],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": os.environ['client_x509_cert_url']
-})
+}
+
+cred = credentials.Certificate(certificate)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
